9ad6857a9fe3bb5f18c2026f569c552c19890c90
theodore.therone@gmail.com <aristotle@ip-10-150-82-116.ec2.internal>
2015-01-29 10:51:34 +0000

    new agency option

--- aristotle_ddi_utils/templatetags/ddi_tags.py
+++ aristotle_ddi_utils/templatetags/ddi_tags.py
@@ -3,9 +3,13 @@ register = template.Library()
 
 @register.simple_tag
 def make_urn(item):
+
+    from django.conf import settings
+    agency = getattr(settings, 'ARISTOTLE_DDI_AGENCY', "example.com")
+
     if item.version:
         version = item.version
     else:
         version = "1.0.0"
-    urn = 'urn:ddi:%s:%s:%s'%('example.com',item.id,version)
+    urn = 'urn:ddi:%s:%s:%s'%(agency,item.id,version)
     return urn
