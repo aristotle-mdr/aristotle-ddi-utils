b84ad6bf2ac9121263cf7fa7bdba86707bf43411
theodore.therone@gmail.com <aristotle@ip-10-138-111-216.ec2.internal>
2015-06-05 19:39:54 +0000

    added valuedomains

--- aristotle_ddi_utils/templatetags/ddi_tags.py
+++ aristotle_ddi_utils/templatetags/ddi_tags.py
@@ -13,3 +13,26 @@ def make_urn(item):
         version = "1.0.0"
     urn = 'urn:ddi:%s:%s:%s'%(agency,item.id,version)
     return urn
+
+@register.filter
+def valuedomain_dditype(item):
+
+    type_map = {
+        #"":"Code",
+        "Date/Time":"DateTime",
+        #"":"ExternalCategory",
+        #"":"GeographicLocationCode",
+        #"":"GeographicStructureCode",
+        "Number":"Numeric",
+        "Currency":"Numeric",
+        #"":"Scale",
+        "String":"Text"
+    }
+    value_type = "Text"
+    num_values = item.permissiblevalue_set.all().count()
+    if item.data_type and num_values == 0:
+        value_type = type_map.get(item.data_type.name)
+    elif num_values > 0:
+        value_type = "Code"
+
+    return "r:Managed" + value_type + "Representation"
