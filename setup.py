44c4f7d6effcbbda1feda410196c126b935719d2
Samuel Spencer <theodore.therone@gmail.com>
2015-12-09 20:20:18 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-aristotle-ddi',
+    name='django-aristotle-ddi-utils',
     version='0.1',
-    packages=['aristotle_ddi'],
+    packages=['aristotle_ddi_utils'],
     include_package_data=True,
     license='BSD Licence',
     description='',
     long_description=README,
-    url='https://github.com/aristotle-mdr/aristotle-ddi',
+    url='https://github.com/aristotle-mdr/aristotle-ddi-utils',
     author='Samuel Spencer',
     author_email='sam@sqbl.org',
     classifiers=[
