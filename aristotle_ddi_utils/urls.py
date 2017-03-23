26fef4ca00893a221fffca4feec77319a1697441
Samuel Spencer <theodore.therone@gmail.com>
2017-03-23 04:13:11 +0000

    dj10

--- aristotle_ddi_utils/urls.py
+++ aristotle_ddi_utils/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('aristotle_ddi_utils.views',
+urlpatterns = [
 #These are required for about pages to work. Include them, or custom items will die!
 #    url(r'^about/(?P<template>.+)/?$', views.DynamicTemplateView.as_view(), name="about"),
     url(r'^/?$', TemplateView.as_view(template_name='aristotle_ddi_utils/about_aristotle_ddi.html'), name="about"),
     url(r'^about/?$', TemplateView.as_view(template_name='aristotle_ddi_utils/about_aristotle_ddi.html'), name="about"),
-)
+]
